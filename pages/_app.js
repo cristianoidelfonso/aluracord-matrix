80d72f6e16838cc5e95cd52713f5fba873be46b4
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-01-25 14:36:52 -0300

    add head e title

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 function GlobalStyle(){
     return(
       <style global jsx>{`
@@ -29,10 +31,14 @@ function GlobalStyle(){
 
 function App({ Component, pageProps}){
     return(
-        <>
-            <GlobalStyle />
-            <Component {...pageProps} />
-        </>
+      <>
+        <Head>
+          <title>Ideltech Soluções - Aluracord</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
     );
 }
 
